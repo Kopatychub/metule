5c727f6b1fdf884c2532a9b70aa3f690096f5343
Kopatychub <119747234+Kopatychub@users.noreply.github.com>
2024-11-22 17:03:28 +0300

    finish Onboarding

--- app/src/main/java/com/example/matuleme/Onboarding.kt
+++ app/src/main/java/com/example/matuleme/Onboarding.kt
@@ -1,16 +1,22 @@
 package com.example.matuleme
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ViewAnimator
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 
 class Onboarding : AppCompatActivity() {
     private lateinit var viewPager: ViewPager2
     private lateinit var startButton: Button
     private lateinit var adapter: OnboardigAdapter
+    private lateinit var indicatorLayout: LinearLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,7 @@ class Onboarding : AppCompatActivity() {
 
         viewPager = findViewById(R.id.viewpager)
         startButton = findViewById(R.id.start_button)
+        indicatorLayout = findViewById(R.id.indicatorLayout)
 
         adapter = OnboardigAdapter(this)
 
@@ -27,10 +34,12 @@ class Onboarding : AppCompatActivity() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 updateButton(position)
+                updateIndicator(position)
             }
         })
 
         updateButton(0)
+        setIndicators(adapter.itemCount)
 
         startButton.setOnClickListener {
             if (viewPager.currentItem == adapter.itemCount - 1) {
@@ -41,16 +50,65 @@ class Onboarding : AppCompatActivity() {
         }
     }
 
+    private fun animateIndicator(indicator: View, targetWidth: Int) {
+        val initialWidth = indicator.width
 
+        val animator = ValueAnimator.ofInt(initialWidth, targetWidth).apply {
+            addUpdateListener { animation ->
+                indicator.layoutParams = indicator.layoutParams.apply {
+                    width = animation.animatedValue as Int
+                }
+                indicator.requestLayout()
+            }
+        }
+
+        animator.duration = 200
+        animator.start()
+    }
+
+    private fun updateIndicator(position: Int) {
+        for (i in 0 until indicatorLayout.childCount){
+            val indicator = indicatorLayout.getChildAt(i) as View
+
+            if (position == i) {
+                indicator.setBackgroundResource(R.drawable.active_line_indicator)
+                animateIndicator(indicator, 120)
+
+            } else {
+                indicator.setBackgroundResource(R.drawable.inactive_line_indicator)
+                animateIndicator(indicator, 80)
+            }
+        }
+    }
+
+    private fun setIndicators(count: Int){
+        for (i in 0 until count){
+            val indicator = View(this).apply {
+                layoutParams = LinearLayout.LayoutParams (
+                    LinearLayout.LayoutParams.WRAP_CONTENT,
+                    LinearLayout.LayoutParams.WRAP_CONTENT
+                ).apply {
+                    setMargins(10,0, 10, 0)
+
+                    height = 20
+                    width = 80
+
+                }
+                background = ContextCompat.getDrawable(context, R.drawable.inactive_line_indicator)
+
+            }
+            indicatorLayout.addView(indicator)
+        }
+        updateIndicator(0)
+    }
 
     private fun updateButton(position: Int) {
         when (position) {
             adapter.itemCount - 3 -> {
-                startButton.text = "Начать" // Изменение текста для последней страницы
+                startButton.text = "Начать"
             }
-
             else -> {
-                startButton.text = "Далее" // Изменение текста для других страниц
+                startButton.text = "Далее"
             }
         }
     }
@@ -58,5 +116,6 @@ class Onboarding : AppCompatActivity() {
     private fun startMainActivity() {
         val intent = Intent(this@Onboarding, MainActivity::class.java)
         startActivity(intent)
+        finish()
     }
 }
